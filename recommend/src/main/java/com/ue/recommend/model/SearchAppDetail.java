b1713af78055d104386bb52957a17be2fb6fd995
tom <120276969@qq.com>
2017-11-25 16:35:13 +0800

    do not save search result in db

--- recommend/src/main/java/com/ue/recommend/model/SearchAppDetail.java
+++ recommend/src/main/java/com/ue/recommend/model/SearchAppDetail.java
@@ -1,20 +1,15 @@
 package com.ue.recommend.model;
 
-import android.arch.persistence.room.Entity;
-import android.arch.persistence.room.PrimaryKey;
-
 import com.ue.adapterdelegate.Item;
 import com.ue.recommend.util.GsonHolder;
 
 /**
  * Created by hawk on 2017/11/25.
  */
-@Entity
 public class SearchAppDetail implements Item {
     public String name;
     public String iconUrl;
     public String editorIntro;
-    @PrimaryKey
     private String pName;
     public transient String appUrl;
 
